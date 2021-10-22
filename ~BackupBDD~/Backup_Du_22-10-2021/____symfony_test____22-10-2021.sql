c26538e4ee8cfea4b87958f69ad1bd0d5ad93bf4
Pierre62 <courquin.pierre62@gmail.com>
2021-10-22 10:53:43 +0200

    22/10/2021 10:53:43,26

--- ~BackupBDD~/Backup_Du_22-10-2021/____symfony_test____22-10-2021.sql
+++ ~BackupBDD~/Backup_Du_22-10-2021/____symfony_test____22-10-2021.sql
@@ -32,12 +32,12 @@ DROP TABLE IF EXISTS `categories`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `categories` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `categorie_id` int(11) DEFAULT NULL,
+  `categorie_mere_id` int(11) DEFAULT NULL,
   `libelle_categorie` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `IDX_3AF34668BCF5E72D` (`categorie_id`),
-  CONSTRAINT `FK_3AF34668BCF5E72D` FOREIGN KEY (`categorie_id`) REFERENCES `categories` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  KEY `IDX_3AF34668665D6AAC` (`categorie_mere_id`),
+  CONSTRAINT `FK_3AF34668665D6AAC` FOREIGN KEY (`categorie_mere_id`) REFERENCES `categories` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -46,7 +46,7 @@ CREATE TABLE `categories` (
 
 LOCK TABLES `categories` WRITE;
 /*!40000 ALTER TABLE `categories` DISABLE KEYS */;
-INSERT INTO `categories` VALUES (1,NULL,'');
+INSERT INTO `categories` VALUES (2,NULL,'NTM'),(3,4,'Produit laitiers'),(4,NULL,'Ordinateur');
 /*!40000 ALTER TABLE `categories` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -71,8 +71,36 @@ CREATE TABLE `doctrine_migration_versions` (
 
 LOCK TABLES `doctrine_migration_versions` WRITE;
 /*!40000 ALTER TABLE `doctrine_migration_versions` DISABLE KEYS */;
-INSERT INTO `doctrine_migration_versions` VALUES ('DoctrineMigrations\\Version20211019165753','2021-10-19 16:58:58',95),('DoctrineMigrations\\Version20211019175250','2021-10-19 17:55:06',45),('DoctrineMigrations\\Version20211019175934','2021-10-19 17:59:58',154),('DoctrineMigrations\\Version20211020163858','2021-10-20 16:39:11',199),('DoctrineMigrations\\Version20211020173141','2021-10-20 17:31:50',100);
+INSERT INTO `doctrine_migration_versions` VALUES ('DoctrineMigrations\\Version20211019165753','2021-10-19 16:58:58',95),('DoctrineMigrations\\Version20211019175250','2021-10-19 17:55:06',45),('DoctrineMigrations\\Version20211019175934','2021-10-19 17:59:58',154),('DoctrineMigrations\\Version20211020163858','2021-10-20 16:39:11',199),('DoctrineMigrations\\Version20211020173141','2021-10-20 17:31:50',100),('DoctrineMigrations\\Version20211021160847','2021-10-21 16:08:54',104),('DoctrineMigrations\\Version20211021163258','2021-10-21 16:33:06',46),('DoctrineMigrations\\Version20211021164712','2021-10-21 16:47:21',57);
 /*!40000 ALTER TABLE `doctrine_migration_versions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `roles` json NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `is_verified` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQ_8D93D649E7927C74` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'courquin.pierre62@gmail.com','[]','$2y$13$/z2QgRrraITqJczRGfQqIO8cvQTvslzUiAwUI78GWwxb5rtPZv.Bq',0),(2,'courquin.pierre69@gmail.com','[]','$2y$13$m7h6uxO/OkcUCAj3QqeF6u77/WVFStAX7.WwWX6NEXaduqvvli7Bq',0);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -84,4 +112,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-10-21 17:27:02
+-- Dump completed on 2021-10-22 10:52:43
