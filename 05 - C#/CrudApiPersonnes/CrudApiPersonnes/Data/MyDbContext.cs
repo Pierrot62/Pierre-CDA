9fc6b4357352527b2747375945c94370ee3e5d56
Pierrot62 <courquin.pierre62@gmail.com>
2021-11-23 13:20:24 +0100

    13h20

--- 05 - C#/CrudApiPersonnes/CrudApiPersonnes/Data/MyDbContext.cs
+++ 05 - C#/CrudApiPersonnes/CrudApiPersonnes/Data/MyDbContext.cs
@@ -1,11 +1,16 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace CrudApiPersonnes.Data
 {
-    public class MyDbContext
+    public class MyDbContext:DbContext
     {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) 
+        { 
+        
+        }
     }
 }
---
+++ 05 - C#/CrudApiPersonnes/CrudApiPersonnes/Models/DbModels/Personne.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+#nullable disable
+
+namespace CrudApiPersonnes.Models.DbModels
+{
+    public partial class Personne
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public int Age { get; set; }
+    }
+}
