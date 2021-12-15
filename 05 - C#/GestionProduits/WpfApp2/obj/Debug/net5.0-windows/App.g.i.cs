36b2caf7622fdce87139076e13a293e3d2a6ba5f
Pierrot62 <courquin.pierre62@gmail.com>
2021-12-15 17:36:36 +0100

    17h36

--- 05 - C#/GestionProduits/WpfApp2/obj/Debug/net5.0-windows/App.g.i.cs
+++ 05 - C#/GestionProduits/WpfApp2/obj/Debug/net5.0-windows/App.g.i.cs
@@ -59,7 +59,7 @@ namespace GestionProduits {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/GestionProduits;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/GestionProduits;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
